2f8bad096647d0a7f65e42510ece8bb320c1b5b4
goneal26 <g.oneal26@ncf.edu>
2024-04-06 11:52:56 -0400

    Created and registered blurb and outlet models
    Models/database tables for blurbs and outlets. I also added myself as a superuser so I can add outlets through the admin console, and generated our first requirements.txt

--- features/models.py
+++ features/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from PIL import Image # pillow for handling outlet image logo
 # Create your models here.
+
+# model for news outlets
+class Outlet(models.Model):
+	rss_url = models.CharField(max_length=255) # RSS url
+	name = models.CharField(max_length=255) # outlet name
+	logo = models.ImageField(default= 'default.png', upload_to='outlet_logos') # TODO setup media folder
+
+	def __str__(self):
+		return f'Outlet: {self.name}'
+
+# model for blurb posts
+class Blurb(models.Model):
+	title = models.CharField(max_length=255) # article title
+	description = models.TextField(blank=True, default="") # article description
+	outlet = models.ForeignKey(Outlet, on_delete=models.CASCADE, blank=True, null=True) # news outlet poster
+	link = models.CharField(max_length=255) # external url for article, I think 255 is plenty
+	# upvotes = models.ManyToManyField(User, related_name='upvoted_blurbs', blank=True)
+	# downvotes = models.ManyToManyField(User, related_name='downvoted_blurbs', blank=True)
+	# TODO: getting upvotes/downvotes working properly requires User system, for next sprint
+
+
