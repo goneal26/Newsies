c7546252c54f99f590d5a5a950944e80560ad636
goneal26 <g.oneal26@ncf.edu>
2024-04-10 21:15:20 -0400

    RSS database loading functional!
    Successfully added automated loading of RSS feeds into blurbs, running once daily at 7AM to fetch the news feed for the day. Now to just render these as html

--- features/jobs.py
+++ features/jobs.py
@@ -2,9 +2,12 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from .rss import RSSFetcher
 from pytz import timezone
+from datetime import datetime
 
 def start(): # help from https://apscheduler.readthedocs.io/en/3.x/userguide.html#code-examples
 	scheduler = BackgroundScheduler()
-	scheduler.add_job(RSSFetcher.update_db, 'cron', hour=7, timezone=timezone('EDT')) # executes every day at 7AM EDT
+	scheduler.add_job(RSSFetcher.update_db, 'cron', hour=7) # executes every day at 7AM EDT
+	
+	# scheduler.add_job(RSSFetcher.update_db, 'date', run_date=datetime.now())
 	scheduler.start()
 	# starts a scheduler to run the RSS fetching stuff once per certain time of day (hence the arg 'cron')
