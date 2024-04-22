ba300a35a93914293d91492c1fff8c927cf69f0e
goneal26 <g.oneal26@ncf.edu>
2024-04-22 14:34:16 -0400

    User Authentication
    Initial user authentication, login, logout, registration, and profile updating complete!
    Only issues still present involve issues with the bootstrap CSS not loading/interfering with preexisting styles, and for some reason the update profile image system on the profile page doesn't work properly (won't update PFP), but otherwise everything works

--- features/views.py
+++ features/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import *
 
-# Create your views here.
-
+@login_required
 def discovery_page(request):
     all_blurbs = Blurb.objects.select_related('outlet').all()
-
     
     context = {
         'blurbs': all_blurbs,
-    }
-
+    } # TODO filtering
+    
     return render(request, 'discovery.html', context)
